b319ca9fcac8af8873c2fb18cd2e2fe89fafaaab
Matthew Greco <mgreco2112@tutanota.com>
2023-07-18 13:13:42 -0400

    moved post mapping for new song into RoomController. Changed RoomController delete mapping to delete songs after room has been deleted

--- src/main/java/com/example/demo/controllers/RoomController.java
+++ src/main/java/com/example/demo/controllers/RoomController.java
@@ -3,9 +3,11 @@ package com.example.demo.controllers;
 import com.example.demo.models.Guest;
 import com.example.demo.models.Room;
 import com.example.demo.models.SpotifySong;
+import com.example.demo.payload.request.NewSongRequest;
 import com.example.demo.repositories.GuestRepository;
 import com.example.demo.repositories.RoomRepository;
 import com.example.demo.repositories.SpotifySongRepository;
+import org.apache.coyote.Response;
 import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -58,6 +61,23 @@ public class RoomController {
         guestRepository.save(newHost);
 
         return new ResponseEntity<>(newRoom, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}/new/song")
+    public ResponseEntity<Room> createSongInRoom(@PathVariable Long id, @RequestBody NewSongRequest request) {
+        Room selRoom = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        SpotifySong newSong = new SpotifySong(
+                request.getSpotifySongURL(),
+                request.getName(),
+                selRoom
+        );
+
+        songRepository.save(newSong);
+
+        selRoom.addSong(newSong);
+
+        return ResponseEntity.ok(selRoom);
     }
 
     @PutMapping("/update/host/{hId}/room/{rId}")
@@ -130,6 +150,8 @@ public class RoomController {
     @DeleteMapping("/delete/id/{id}")
     public ResponseEntity<String> deleteRoomById(@PathVariable Long id) {
 
+        List<SpotifySong> songsList = new ArrayList<>();
+
         Room selRoom = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         Guest host = guestRepository.findById(selRoom.getHost().getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -147,10 +169,15 @@ public class RoomController {
         for (SpotifySong song : selRoom.getSongs()) {
             song.setRoom(null);
             songRepository.save(song);
+            songsList.add(song);
         }
 
         repository.save(selRoom);
         repository.delete(selRoom);
+
+        for (int i = 0; i < songsList.size(); i++) {
+            songRepository.delete(songsList.get(i));
+        }
 
         return ResponseEntity.ok("Deleted Room");
     }
